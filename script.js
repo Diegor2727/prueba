a8d5054755a68af82f9faff2c5412d3dbf3ace8b
Diegor2727 <diego_r_2727@hotmail.com>
2022-08-03 17:16:08 -0300

    realizando los primeros ejercicios

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-alert("Hola mundo!!!");
+/*alert("Hola mundo!!!");
+console.log(typeof"probando");
+console.log(typeof null);
+
+let miVariable = "que onda";
+console.log(typeof miVariable);
+const constante = "es una constante";
+console.log(constante);
+var variableVar = 45;
+
+console.log(miVariable, constante, variableVar);
+
+console.log(`Le preguente ${miVariable} si ${constante} y me dijo que era ${variableVar}`);
+
+var mensaje = `por fin lo logre!!!`;
+
+console.log(mensaje);*/
+
+//const firtName= prompt("Ingresa tu nombre: "); 
+
+//alert(`Hola ${firtName}`);
+
+//const firstNumber = prompt("Ingresa un numero: ");
+
+//const seconNumber = prompt("Ingresa otro numero: ");
+
+//alert(`La suma de ${firstNumber} y ${seconNumber} es igual a ${+firstNumber + +seconNumber}`);
+
+//alert(`El doble de la suma de ${firstNumber} y ${seconNumber} es ${(+firstNumber + +seconNumber) * 2}`); 
+
+//const width = prompt("Ingresa un ancho de la habitacion:");
+
+//const heigh = prompt("Ingresa el alto de la habitacion:");
+ 
+//const result = width * heigh; 
+
+//alert(`La Superficie de tu habitacion es ${result}`);
+
+const firstName = prompt("Ingrese su mombre:");
+
+const lastName = prompt("Ingrese su apellido:");
+
+const age = prompt("Ingrese su edad:");
+
+//alert(`Tu nomebre en mayusculas es ${firstName.toUpperCase()}`);
+
+alert(`Tus datos completos son: 
+Nombre: ${firstName}
+Apellido: ${lastName}
+Edad: ${age}`);
+
+
+
+
+
+
+
+
+
